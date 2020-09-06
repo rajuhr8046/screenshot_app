2d907e55a8452617cab71e1a964d216c90418a00
rajuhr8046 <56189743+rajuhr8046@users.noreply.github.com>
2020-09-06 11:33:47 +0530

    a line is removed

--- screenshot.py
+++ screenshot.py
@@ -5,7 +5,6 @@ import tkinter as tk
 def screenshot():
     name = int(round(time.time() * 1000))
     name = 'D:\screenshots_data\{}.png'.format(name)
-#    time.sleep(5)
     img = pyautogui.screenshot(name)
     img.show()
 
